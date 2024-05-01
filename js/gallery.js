9e10b44de9dbde96cee49c762601e860cb58c43e
RayceBrewer <156971691+RayceBrewer@users.noreply.github.com>
2024-05-01 15:43:46 -0500

    a

--- js/gallery.js
+++ js/gallery.js
@@ -3,9 +3,19 @@ $( document ).ready(function() {
     //console.log( "ready!" );
 
     $("#blinn-gallery").nanogallery2({
-        ...
-        thumbnailHeight:  200,
-        thumbnailWidth:   200
-      });
+        thumbnailWidth: "300",
+        thumbnailHeight: "auto",
+        thumbnailBorderVertical: 0,
+        thumbnailBorderHorizontal: 0,
+        thumbnailLabel: {
+            position: "overImageOnBottom",
+            display: false
+        },
+        thumbnailHoverEffect2: "ImageScaleIn80|imageSepiaOff|labelAppear75",
+        thumbnialGutterWidth: 0,
+        thumbnailGutterHeight: 0,
+        thumbnailAlingnment: "center",
+        thumbnailOpenImage: true 
+    });
 
 });
